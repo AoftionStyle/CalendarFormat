3bcba58e3b5773f866b6a108d85573ec576ecdad
Chutipon Pongpanit <chutipon.ppn@gmail.com>
2017-10-02 09:27:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-    document.getElementById('demo').innerHTML = callCalendarCSAccounting()
+    var inputDate = "31-12-2015 23:23:12"
+    document.getElementById('demo').innerHTML = callCalendarCSAccounting(inputDate)
+
+    myFunction()
   });
 
 function myFunction(){
-
+    // var inputDate = "31/12/2015 23:23:12"
+    // document.getElementById('demo').innerHTML = callCalendarCSAccounting(inputDate)
 }
 
 function aoftion(){
